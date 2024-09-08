5ac420a9d607270cdf894efc3149d3e287bddb8d
anushadevelopment <anushadevelopment@gmail.com>
2024-09-08 20:44:15 +0800

    first commit

---
+++ auth-service/src/main/java/com/safalifter/authservice/service/JwtService.java
@@ -0,0 +1,45 @@
+package com.safalifter.authservice.service;
+
+import com.safalifter.authservice.client.UserServiceClient;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class JwtService {
+    private final UserServiceClient userServiceClient;
+    private final CustomUserDetailsService customUserDetailsService;
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    public String generateToken(String username) {
+        ResponseEntity<UserDto user = userServiceClient.getUserByUsername(username);
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails);
+    }
+
+    private String createToken(Map<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuer(userDetails.getAuthorities().iterator().next().getAuthority())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
